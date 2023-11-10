77a75c13dea7c19e3df9efd4d820933b7903e267
Sydnyepie <smhilton04@gmail.com>
2023-11-09 22:39:07 -0800

    created attack function and stubs for needed classes

--- battleActions.cpp
+++ battleActions.cpp
@@ -1,1 +1,59 @@
 #include "battleActions.h"
+#include <stdlib.h>
+#include <time.h>
+
+int battleActions::attack(EnemyStub attackedEnemy){ 
+    srand(time);
+    if((rand() % 25) <= attackedEnemy.getEvasion()){
+        int addedWeaponDmg = (rand() % this->weaponDmg);
+        int totalDamage = addedWeaponDmg + this->attack;
+
+        if((rand() %  100) + 1 <= 7){
+            totalDamage *= 2;
+        }
+
+        return totalDamage;
+    }
+    else{
+        return 0;
+    }
+}
+
+    // int battleActions::defend(){
+
+    // }
+
+    // void battleActions::useItem(){
+
+
+    // }
+/*
+Damage to player character is done by doing srand % (baseAttack/#) and adding the highest modifier 
+to the baseAttack which is the damage, the players defenses are then subtracted from the attack to 
+see how much health is lost; bigger monsters have more chances to do big damage but the lil rats for 
+example only have one shot at it (the # is the bit from the base Attack to modulo to (S)
+*/
+
+/*
+class PlayerCharacterStub{
+    private:
+        int attack;
+        int defense;
+        int weaponDmg;
+
+    public:
+    
+    friend class battleActions;
+};
+
+
+class EnemyStub{
+    private:
+        int attack;
+        int evasion; 
+
+    public:
+        int getEvasion() {return evasion;}
+        int dealDamage();
+};
+*/
--- battleActions.h
+++ battleActions.h
@@ -1,6 +1,29 @@
 class battleActions{ //friend to playerCharacter class
     public:
-        virtual int attack();
-        virtual int defend();
-        void useItem();
-}
+        virtual int attack(EnemyStub);
+        //virtual int defend();
+        //void useItem();
+};
+
+
+class PlayerCharacterStub{
+    private:
+        int attack;
+        int defense;
+        int weaponDmg;
+
+    public:
+    
+    friend class battleActions;
+};
+
+
+class EnemyStub{
+    private:
+        int attack;
+        int evasion; 
+
+    public:
+        int getEvasion() {return evasion;}
+        int dealDamage();
+};
