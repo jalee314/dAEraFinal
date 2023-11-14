b6a39520995d3f26b6bdbba8decd6af59da45c6d
jalee314 <jasonklee1003@gmail.com>
2023-11-14 13:19:50 -0800

    Fixed missing semicolon and std::

--- src/NPC.cpp
+++ src/NPC.cpp
@@ -10,13 +10,16 @@ void NPC::addQuestion(const std::string& question) {
 }
 
 void NPC::printName() {
-    cout << this->name << "\n";
+    std::cout << this->name << "\n";
 }
 
-void NPC::printResponse(int index) {
+void NPC::printResponse(int index) {xw
     if(index >= 0 && index < responses.size()) {
-        std::cout << responses[questionIndex] << "\n"
+        std::cout << responses[questionIndex] << "\n";
     }
-    else std:: cout << "I am unable to answer your question." << "\n"
+    else std:: cout << "I am unable to answer your question." << "\n";
 }
 
+/* this is a temporary implementation from my understanding of what the NPC character is to do 
+basically printResponse is totally up for change if its dependent on an algorithm that 
+finds the most appropriate response to a question */
