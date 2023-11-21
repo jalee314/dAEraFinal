b59805efcbd02bbb1f30e63ce8b725af4ebdd6c5
jalee314 <jasonklee1003@gmail.com>
2023-11-21 15:56:37 -0800

    Added implementation of Item class

---
+++ header/Item.h
@@ -0,0 +1,48 @@
+#ifndef ITEM_H 
+#define ITEM_H
+#include <string>
+
+class IAssistive {
+public:
+    virtual int getAssistance() const = 0; // Pure virtual function
+    virtual ~IAssistive() = default;
+};
+
+/*thought about splitting up the virtual functions into two separate 
+classes for useItem and printItem due to SRP, but this class in my opinion
+can be described as "A class that controls item usage for the player character".
+so even though functions serve different purposes, I thought at the end of they day it 
+fits under the same umbrella
+*/
+
+class Item {
+public:
+    Item(const std::string& itemName): name(itemName){}
+    virtual int useItem() = 0;
+    virtual void printItem() = 0;
+protected:
+    std::string name;
+};
+
+class HelpItem: public Item, public IAssistive {
+public:
+    HelpItem(const std::string& itemName, int assistanceValue): Item(itemName), assistance(assistanceValue){}
+    virtual int useItem();
+    virtual void printItem();
+    virtual int getAssistance() const{return assistance;}
+private:
+    int assistance;
+};
+
+class Weapon: public Item {
+public:
+    Weapon(const std::string& itemName, int damageValue): Item(itemName), damage(damageValue){}
+    virtual int useItem();
+    virtual void printItem();
+private:
+    int damage;
+};
+
+#endif //ITEM_H
+
+
