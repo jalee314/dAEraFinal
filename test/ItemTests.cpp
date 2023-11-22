2cc0491713359ff625e7d9d7150582ae13dafbc5
jalee314 <jasonklee1003@gmail.com>
2023-11-21 16:48:23 -0800

    changed the order of the unit tests a little and got rid of the seg faults i was getting before i added the changes so...i guess im reversing the last changes

--- test/ItemTests.cpp
+++ test/ItemTests.cpp
@@ -8,25 +8,25 @@ TEST(ItemsTestSuite, useHelpItem) {
     EXPECT_EQ(vitamins.useItem(), 5);
 }
 
+TEST(ItemsTestSuite, useWeapon) {
+    Weapon musket("Musket", 10); 
+    EXPECT_EQ(musket.useItem(), 10);
+}
+
 TEST(ItemsTestSuite, printHelpItem) {
     HelpItem vitamins("Vitamins", 10); 
     std::stringstream buffer;
-    std::streambuf* prev = std::cout.rdbuf(buffer.rdbuf()); 
+    std::cout.rdbuf(buffer.rdbuf()); 
     vitamins.printItem();
-    std::cout.rdbuf(prev);
     EXPECT_EQ("Item: Vitamins, Assistance: 10\n", buffer.str());
 }
 
 TEST(ItemsTestSuite, printWeapon) {
     Weapon musket("Musket", 10); 
     std::stringstream buffer;
-    std::streambuf* prev = std::cout.rdbuf(buffer.rdbuf()); 
+    std::cout.rdbuf(buffer.rdbuf()); 
     musket.printItem();
-    std::cout.rdbuf(prev);
     EXPECT_EQ("Weapon: Musket, Damage: 10\n", buffer.str());
 }
 
-TEST(ItemsTestSuite, useWeapon) {
-    Weapon musket("Musket", 10); 
-    EXPECT_EQ(musket.useItem(), 10);
-}
+
