76782937e4a0fd54ed9a404e03db3fc796d231d6
jalee314 <jasonklee1003@gmail.com>
2023-12-07 12:31:16 -0800

    Forgot to add this to last commit

--- header/playerCharacter.h
+++ header/playerCharacter.h
@@ -12,14 +12,17 @@ class PlayerCharacter : public IEntity{
         string difficulty;
         InventoryManagement inventory;
         InventoryDisplay inventoryDisplay;
-        Weapon weapon;
+        Weapon* weapon;
     public:
-        PlayerCharacter(int health, int attack, int defense, string diff, Weapon& charWeapon);
+        PlayerCharacter(int health, int attack, int defense, string diff, Weapon* charWeapon);
         virtual void printStatus();
         virtual const string getDifficulty() { return difficulty; }
         virtual const int getHealth() { return health; }
         virtual const int getAttack() { return attack; }
         virtual const int getDefense() { return defense; }
+        void setAttack(Weapon* weapon) {attack = weapon->getDamage();} //switching weapon
+        Weapon* getWeapon() {return weapon;}
+        void setWeapon(Weapon* newWeapon) {weapon = newWeapon;} 
         void addToInventory(Item* item);
         void removeFromInventory(Item* item);
         bool itemInInventory(const std::string& item);
@@ -34,20 +37,20 @@ class PlayerCharacter : public IEntity{
 
 class Soldier : public PlayerCharacter{ //for all characters they are given default stats
     public:
-        Soldier(Weapon& weapon) : PlayerCharacter(120, 7, 5, "Easy", weapon){            
+        Soldier(Weapon* weapon) : PlayerCharacter(120, 7, 5, "Easy", weapon){            
         }
         void buffAttack();
 };
 
 class Engineer : public PlayerCharacter{
     public:
-        Engineer(Weapon& weapon) : PlayerCharacter(100, 5, 4, "Medium", weapon){
+        Engineer(Weapon* weapon) : PlayerCharacter(100, 5, 4, "Medium", weapon){
         }
         void buffDefense();
 };
 
 class Biologist : public PlayerCharacter{
     public:
-        Biologist(Weapon& weapon) : PlayerCharacter(80, 4, 3, "Hard", weapon){}
+        Biologist(Weapon* weapon) : PlayerCharacter(80, 4, 3, "Hard", weapon){}
         void buffHealth();
 };
