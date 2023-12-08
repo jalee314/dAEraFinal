a5e76803d0b6bf3ba26ba3786a46396407dce7f2
jalee314 <120427188+jalee314@users.noreply.github.com>
2023-12-08 06:57:35 -0800

    Delete src/RNG.cpp

--- src/RNG.cpp
+++
@@ -1,7 +0,0 @@
-#include <random>
-
-int rng() {
-        std::mt19937 gen(std::random_device rd());
-        std::uniform_int_distribution<int> distribution(1, 100);
-        return distribution(gen);    
-    }
