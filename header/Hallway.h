3a74c5af4c88b043369670165c3d8c94166688b0
jalee314 <jasonklee1003@gmail.com>
2023-12-08 10:26:47 -0800

    Added valgrind image to repo. also made slight changes to hallway and environment (very slight)
    #

--- header/Hallway.h
+++ header/Hallway.h
@@ -6,6 +6,9 @@
 class Hallway : public Environment {
 public:
    Hallway(const std::string& name);
+    virtual void setItemInRoom(Item* item){}
+    virtual void setEnemyInRoom(EnemyStatus* enemy){}
+    virtual void setPageInRoom(Page* page){}
    virtual ~Hallway();
 };
 
